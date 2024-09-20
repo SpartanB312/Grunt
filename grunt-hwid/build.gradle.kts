643564856c0007883d1d1eea4fc9480a23fa6b03
NoobSex <61795895+SpartanB312@users.noreply.github.com>
2024-09-21 05:59:59 +0800

    HWID Main

--- grunt-hwid/build.gradle.kts
+++ grunt-hwid/build.gradle.kts
@@ -5,3 +5,13 @@ plugins {
 repositories {
     mavenCentral()
 }
+
+tasks {
+    jar {
+        manifest {
+            attributes(
+                "Main-Class" to "net.spartanb312.grunt.hwid.HWID"
+            )
+        }
+    }
+}
