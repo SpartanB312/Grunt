60f5dbb20a16c31c5816781fe26ad7b8d0cc3210
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-11-09 00:50:33 -0500

    FieldInsnNode, FrameNode factory

--- grunt-asm/src/main/kotlin/net/spartanb312/grunteon/asm/tree/insn/InsnList.kt
+++ grunt-asm/src/main/kotlin/net/spartanb312/grunteon/asm/tree/insn/InsnList.kt
@@ -1,3 +1,5 @@
+@file:Suppress("FunctionName")
+
 package net.spartanb312.grunteon.asm.tree.insn
 
 import net.spartanb312.grunteon.asm.MethodVisitor
@@ -9,8 +11,24 @@ interface InsnList : List<BaseInsnNode> {
     }
 }
 
-@Suppress("FunctionName")
 interface InsnListBuilder {
+    fun FieldInsnNode(
+        opcode: Int,
+        owner: String,
+        name: String,
+        desc: String,
+        visibleTypeAnnotations: List<TypeAnnotationNode> = emptyList(),
+        invisibleTypeAnnotations: List<TypeAnnotationNode> = emptyList()
+    )
+
+    fun FrameNode(
+        type: Int,
+        numLocal: Int,
+        local: List<Any>?,
+        numStack: Int,
+        stack: List<Any>?
+    )
+
     fun MethodInsnNode(
         opcode: Int,
         owner: String,
@@ -22,6 +40,38 @@ interface InsnListBuilder {
 
     fun build(): InsnList
 }
+
+fun InsnListBuilder.FieldInsnNode(
+    fieldInsnNode: FieldInsnNode,
+    opcode: Int = fieldInsnNode.opcode,
+    owner: String = fieldInsnNode.owner,
+    name: String = fieldInsnNode.name,
+    desc: String = fieldInsnNode.desc,
+    visibleTypeAnnotations: List<TypeAnnotationNode> = fieldInsnNode.visibleTypeAnnotations,
+    invisibleTypeAnnotations: List<TypeAnnotationNode> = fieldInsnNode.invisibleTypeAnnotations
+) = FieldInsnNode(
+    opcode,
+    owner,
+    name,
+    desc,
+    visibleTypeAnnotations,
+    invisibleTypeAnnotations
+)
+
+fun InsnListBuilder.FrameNode(
+    frameNode: FrameNode,
+    type: Int = frameNode.frameType,
+    numLocal: Int = frameNode.local?.size ?: -1,
+    local: List<Any>? = frameNode.local,
+    numStack: Int = frameNode.stack?.size ?: -1,
+    stack: List<Any>? = frameNode.stack
+) = FrameNode(
+    type,
+    numLocal,
+    local,
+    numStack,
+    stack
+)
 
 fun InsnListBuilder.MethodInsnNode(
     methodInsnNode: MethodInsnNode,
