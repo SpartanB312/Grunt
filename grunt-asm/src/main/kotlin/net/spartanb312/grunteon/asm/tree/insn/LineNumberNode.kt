70bfa0f44789a59af7c0f39dfaa29dea6782364b
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-11-11 22:04:55 -0500

    LineNumberNode

---
+++ grunt-asm/src/main/kotlin/net/spartanb312/grunteon/asm/tree/insn/LineNumberNode.kt
@@ -0,0 +1,26 @@
+package net.spartanb312.grunteon.asm.tree.insn
+
+import net.spartanb312.grunteon.asm.MethodVisitor
+import org.objectweb.asm.tree.AbstractInsnNode
+
+/**
+ * A node that represents a line number declaration. These nodes are pseudo instruction nodes in
+ * order to be inserted in an instruction list.
+ *
+ * @author Eric Bruneton
+ * @author Luna
+ */
+interface LineNumberNode : IBaseInsnNode {
+    /** A line number. This number refers to the source file from which the class was compiled.  */
+    val line: Int
+
+    /** The first instruction corresponding to this line number.  */
+    val start: LabelNode
+
+    override val type: Int
+        get() = AbstractInsnNode.LINE
+
+    override fun accept(methodVisitor: MethodVisitor) {
+        methodVisitor.visitLineNumber(line, start.value)
+    }
+}
