9270f96e0c3a70362e8f65953243f44a6e0cbf82
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-11-11 21:23:54 -0500

    FieldNode in InsnList

--- grunt-asm/src/main/kotlin/net/spartanb312/grunteon/asm/tree/InsnList.kt
+++ grunt-asm/src/main/kotlin/net/spartanb312/grunteon/asm/tree/InsnList.kt
@@ -1,94 +1,85 @@
-@file:Suppress("FunctionName")
+@file:Suppress("FunctionName", "SpellCheckingInspection")
 
 package net.spartanb312.grunteon.asm.tree
 
 import net.spartanb312.grunteon.asm.MethodVisitor
-import net.spartanb312.grunteon.asm.tree.insn.BaseInsnNode
-import net.spartanb312.grunteon.asm.tree.insn.FieldInsnNode
-import net.spartanb312.grunteon.asm.tree.insn.FrameNode
-import net.spartanb312.grunteon.asm.tree.insn.IMethodInsnNode
+import net.spartanb312.grunteon.asm.tree.insn.GetStaticInsnNode
+import net.spartanb312.grunteon.asm.tree.insn.IBaseInsnNode
 
-interface InsnList : List<BaseInsnNode> {
+interface InsnList : List<IBaseInsnNode> {
     fun accept(mv: MethodVisitor) {
         forEach { it.accept(mv) }
     }
 }
 
 interface InsnListBuilder {
-    fun FieldInsnNode(
-        opcode: Int,
+    fun GETSTATIC(
         owner: String,
         name: String,
-        desc: String,
-        visibleTypeAnnotations: List<TypeAnnotationNode> = emptyList(),
-        invisibleTypeAnnotations: List<TypeAnnotationNode> = emptyList()
+        desc: String
     )
 
-    fun FrameNode(
-        type: Int,
-        numLocal: Int,
-        local: List<Any>?,
-        numStack: Int,
-        stack: List<Any>?
-    )
-
-    fun MethodInsnNode(
-        opcode: Int,
+    fun PUTSTATIC(
         owner: String,
         name: String,
-        desc: String,
-        visibleTypeAnnotations: List<TypeAnnotationNode> = emptyList(),
-        invisibleTypeAnnotations: List<TypeAnnotationNode> = emptyList()
+        desc: String
+    )
+
+    fun GETFIELD(
+        owner: String,
+        name: String,
+        desc: String
+    )
+
+    fun PUTFIELD(
+        owner: String,
+        name: String,
+        desc: String
     )
 
     fun build(): InsnList
 }
 
-fun InsnListBuilder.FieldInsnNode(
-    fieldInsnNode: FieldInsnNode,
-    opcode: Int = fieldInsnNode.opcode,
-    owner: String = fieldInsnNode.owner,
-    name: String = fieldInsnNode.name,
-    desc: String = fieldInsnNode.desc,
-    visibleTypeAnnotations: List<TypeAnnotationNode> = fieldInsnNode.visibleTypeAnnotations,
-    invisibleTypeAnnotations: List<TypeAnnotationNode> = fieldInsnNode.invisibleTypeAnnotations
-) = FieldInsnNode(
-    opcode,
+fun InsnListBuilder.GETSTATIC(
+    src: GetStaticInsnNode,
+    owner: String = src.owner,
+    name: String = src.name,
+    desc: String = src.desc
+) = GETSTATIC(
     owner,
     name,
-    desc,
-    visibleTypeAnnotations,
-    invisibleTypeAnnotations
+    desc
 )
 
-fun InsnListBuilder.FrameNode(
-    frameNode: FrameNode,
-    type: Int = frameNode.frameType,
-    numLocal: Int = frameNode.local?.size ?: -1,
-    local: List<Any>? = frameNode.local,
-    numStack: Int = frameNode.stack?.size ?: -1,
-    stack: List<Any>? = frameNode.stack
-) = FrameNode(
-    type,
-    numLocal,
-    local,
-    numStack,
-    stack
-)
-
-fun InsnListBuilder.MethodInsnNode(
-    methodInsnNode: IMethodInsnNode,
-    opcode: Int = methodInsnNode.opcode,
-    owner: String = methodInsnNode.owner,
-    name: String = methodInsnNode.name,
-    desc: String = methodInsnNode.desc,
-    visibleTypeAnnotations: List<TypeAnnotationNode> = methodInsnNode.visibleTypeAnnotations,
-    invisibleTypeAnnotations: List<TypeAnnotationNode> = methodInsnNode.invisibleTypeAnnotations
-) = MethodInsnNode(
-    opcode,
+fun InsnListBuilder.PUTSTATIC(
+    src: GetStaticInsnNode,
+    owner: String = src.owner,
+    name: String = src.name,
+    desc: String = src.desc
+) = PUTSTATIC(
     owner,
     name,
-    desc,
-    visibleTypeAnnotations,
-    invisibleTypeAnnotations
+    desc
+)
+
+fun InsnListBuilder.GETFIELD(
+    src: GetStaticInsnNode,
+    owner: String = src.owner,
+    name: String = src.name,
+    desc: String = src.desc
+) = GETFIELD(
+    owner,
+    name,
+    desc
+)
+
+fun InsnListBuilder.PUTFIELD(
+    src: GetStaticInsnNode,
+    owner: String = src.owner,
+    name: String = src.name,
+    desc: String = src.desc
+) = PUTFIELD(
+    owner,
+    name,
+    desc
 )
