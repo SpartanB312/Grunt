7ebc4fa093f15b9f0e42a3dd122ab6e62d4a2bd3
Luna <62033805+Luna5ama@users.noreply.github.com>
2026-06-16 19:56:16 -0400

    Removed unused utils

--- grunt-main/src/main/kotlin/net/spartanb312/grunteon/obfuscator/util/thread/MainScope.kt
+++
@@ -1,8 +0,0 @@
-package net.spartanb312.grunteon.obfuscator.util.thread
-
-import kotlinx.coroutines.CoroutineScope
-
-object MainScope : CoroutineScope by newCoroutineScope(
-    Runtime.getRuntime().availableProcessors(),
-    "Coroutines"
-)
